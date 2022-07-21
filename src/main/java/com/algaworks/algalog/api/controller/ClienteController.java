9fdb87fd63036c51e4da906d74809221ef0e08ae
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-07-21 17:23:40 -0300

    Consulta de clientes no banco utilizando JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,22 +15,12 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager; 
+	
 	
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
-		
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Marco");
-		cliente1.setTelefone("84996159775");
-		cliente1.setEmail("marcoaureliotomaz@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Aurelio");
-		cliente2.setTelefone("84988887777");
-		cliente2.setEmail("aurelio@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente",Cliente.class).getResultList();
 	}
 }
