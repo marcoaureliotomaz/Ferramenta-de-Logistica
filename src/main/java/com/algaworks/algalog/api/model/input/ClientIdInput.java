36733134e2ec2d1e3a129ecc5b6aeca57f00da01
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-07-26 01:09:16 -0300

    Criando mapper com ModelMapper para simplificar a transformação

---
+++ src/main/java/com/algaworks/algalog/api/model/input/ClientIdInput.java
@@ -0,0 +1,15 @@
+package com.algaworks.algalog.api.model.input;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClientIdInput {
+	
+	@NotNull
+	private Long id;
+
+}
